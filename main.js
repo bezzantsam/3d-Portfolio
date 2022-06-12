81e9e55990b05cbdabca0e0d0f6eff5326bfa02a
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-06-12 04:08:47 -0700

    resume details

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ renderer.render(scene, camera);
 // Torus
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const material = new THREE.MeshStandardMaterial({ color: 0x20bd74 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
